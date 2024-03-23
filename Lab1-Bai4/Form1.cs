2d27e94a181f127bf4903609f82dec498bea1fb3
SPRINGPEACHVINH <daoxuan.vinh3@gmail.com>
2024-03-23 15:10:19 +0700

    khinhlanh

---
+++ Lab1-Bai4/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1_Bai4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string result = "";
+        string num = "";     
+        string num_hex = "";
+        string num_bin = "";
+        string num_dec = "";
+        ComboBox Origin_num = new ComboBox();
+        ComboBox Destinate_num = new ComboBox();
+        Regex regex_hex = new Regex("[0-9A-F]");
+        Regex regex_bin = new Regex("[0-1]");
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Origin_num = (ComboBox)sender;           
+        }
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Destinate_num = (ComboBox)sender;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(Origin_num.Text == "Decimal")
+                {
+                    int midle = int.Parse(textBox1.Text);
+                    num_bin = Convert.ToString(midle, 2);
+                    num_hex = Convert.ToString(midle, 16);
+                    num_dec = textBox1.Text;
+                }
+                else if(Origin_num.Text == "Binary")
+                {
+                    int midle = int.Parse(textBox1.Text);
+                    if (regex_bin.IsMatch(textBox1.Text) == false)
+                    {
+                        MessageBox.Show("Invalid Binary Number");
+                        return;
+                    }
+                    else
+                    {
+                        num_bin = textBox1.Text;
+                        num_dec = Convert.ToString(midle, 10);
+                        num_hex = Convert.ToString(midle, 16);
+                    }
+                }
+                else
+                {
+                    string midle = textBox1.Text;
+                    if (regex_hex.IsMatch(midle) == false)
+                    {
+                        MessageBox.Show("Invalid Hex Number");
+                    }
+                    else
+                    {
+
+                    }
+                }
+                if(Destinate_num.Text == "Decimal")
+                {
+                    Result.Text = num_dec;
+                }
+                else if(Destinate_num.Text == "Binary")
+                {
+                    Result.Text = num_bin;
+                }
+                else
+                {
+                    Result.Text = num_hex;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
